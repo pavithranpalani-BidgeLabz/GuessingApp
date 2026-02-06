c1a9c98af657f5f0c8d4027e16c6071f04cdc3b4
pavithranpalani-bridgeLabz <pavithran.palani@bridgelabz.com>
2026-02-07 03:56:00 +0530

    uc1 completed

---
+++ src/main/java/org/example/GameConfig.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Random;
+
+public class GameConfig {
+    final int max = 100;
+    final int min = 1;
+    final int maximumAttempts = 7;
+    final int minimumAttempts = 3;
+
+    public int getMax() {
+        return max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMaximumAttempts() {
+        return maximumAttempts;
+    }
+
+    public int getMinimumAttempts() {
+        return minimumAttempts;
+    }
+
+    int targetNumber;
+
+    public GameConfig() {
+        Random rand = new Random();
+        this.targetNumber = rand.nextInt(max - min + 1) + 1;
+    }
+
+    void showRules(){
+        System.out.println("Guss a Number between " + min + " and "+max);
+        System.out.println("You Have Maximum  Attempts: " + getMaximumAttempts());
+        System.out.println("Hints will be provided after wrong process");
+    }
+}
---
+++ src/main/java/org/example/GuessingApp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class GuissingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Number-Guessing Application");
+
+        GameConfig gameConfig = new GameConfig();
+        gameConfig.showRules();
+
+    }
+}
